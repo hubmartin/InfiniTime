61d9505a5f0389f39327f42313e512c03cedf23d
hubmartin <hub.martin@gmail.com>
2021-07-18 19:09:27 +0200

    1.2.0 Pinetime P8

---
+++ build_script.sh
@@ -0,0 +1,3 @@
+cd build
+cmake -DARM_NONE_EABI_TOOLCHAIN_PATH=/home/martin/gcc-arm-none-eabi-9-2020-q2-update -DNRF5_SDK_PATH=../../nRF5_SDK_15.3.0_59ac345 -DUSE_JLINK=1 -DNRFJPROG=/usr/bin/ ../
+make -j pinetime-app
